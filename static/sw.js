bd567634d22e78554dd5ea4cb50e476ccecb1e18
kenzo0107 <kenzo.tanaka@medpeer.co.jp>
2018-08-09 00:51:02 +0900

    Service Worker で install する静的コンテンツ追加

--- static/sw.js
+++ static/sw.js
@@ -5,21 +5,21 @@ const cacheName = `${cachePrefix}-${version}`
 var urlsToCache = [
   '/',
   '/static/img/favicon.ico',
+  '/static/img/logo.png',
   '/static/css/bootstrap.min.css',
   '/static/css/flickity.org.css',
-  '/static/js/jquery-3.1.0.min.js',
-  '/static/js/flickity.pkgd.min.js',
-  '/static/js/tether.min.js',
-  '/static/img/logo.png',
   '/static/js/async_set_circuit.js',
+  '/static/js/bootstrap.min.js',
+  '/static/js/flickity.pkgd.min.js',
+  '/static/js/jquery-3.1.0.min.js',
   '/static/js/jquery.countdown.min.js',
-  '/static/js/superagent.js'
+  '/static/js/superagent.js',
+  '/static/js/tether.min.js'
 ]
 
 self.addEventListener('install', event => {
   console.log('install')
   event.waitUntil(
-    // open cache
     caches.open(cacheName)
       .then(function(cache) {
         console.log('Opened cache');
@@ -31,7 +31,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   console.log('fetch')
   event.respondWith(
-    // リクエストを見て Service Worker が生成したキャッシュの中に該当するものがあるか探す。
     caches.match(event.request, {
       ignoreSearch:true
     })
